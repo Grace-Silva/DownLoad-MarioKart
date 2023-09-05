76868c56ee8b78244a09dddc0d9d32e507708840
Grace <gracesilvaduarte@gmail.com>
2023-09-04 20:50:31 -0500

    formateo y documentación del proyecto

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Descarga Mario Kart 64
+## Práctica de Todo
+***
+Este repositorio reune los conocimientos y conceptos adquiridos hasta ahora en mi aprendizaje de JavaScript. 
+En el se hacen uso de funciones, temporizadores, DOM, Api de Sonido, eventos, entre otros.
+***
+## Conceptos y Tecnologías.
+- HTML5
+- CSS3
+- Variables
+- Funciones
+- Timers: setTimeOut() & setInterval()
+- Eventos
+- Api de Sonido
+- Document Object Model
+***
+Iniciamos con la pantalla inicial que nos muestra una barra de navegación y un botón para descargar un archivo, el cual encarna la raíz del proyecto, ya que una vez pulsado da ejecución a las funciones e instrucciones para la reproducción del sonido de alerta y descarga del fichero.
+
+![Pantalla Inicial](./img/preview.png)
+![Botón Pulsado](./img/after.png)
+
+Este proyecto se encuentra en desarrollo y preparado para su posterior expansión. Espero haya sea de su agrado.
+
+Autor: [ Grace Silva ](https://github.com/Grace-Silva)
--- src/main.js
+++ src/main.js
@@ -1,62 +1,68 @@
-/* selecciono el botón y lo asigno a una variable. */
+/* selección de elementos: */
 let downloadButton = document.querySelector(".downloadButton");
-/* selecciono el texto de cuenta regresiva */
-let countdown = document.querySelector(".countdown");
-let countdownText = document.querySelector(".countdownText");
 
-let pleaseWaitText = document.querySelector(".pleaseWaitText");
+let countdown = document.querySelector(".countdown"); // span
+
+let countdownText = document.querySelector(".countdownText"); // h1
+
+let pleaseWaitText = document.querySelector(".pleaseWaitText"); // h2
+
 let manualDownloadText = document.querySelector(".manualDownloadText");
+
 let manualDownloadLink = document.querySelector(".manualDownloadLink");
 
 let mainContainer = document.querySelector(".mainContainer");
+
 let rewardAudio = document.getElementById("rewardAudio");
 
-downloadButton.addEventListener("click", () =>{
-    // al clickear sobre nuestro botón este desaparece y las demás funciones se ejecutarán
+// al clickear sobre nuestro botón este desaparece y las demás funciones se ejecutarán:
+
+downloadButton.addEventListener("click", () => {
+  rewardAudio.volume = 0.5;
+  rewardAudio.play();
+
+  setTimeout(() => {
+    // hacemos esperar a la funcion de cuenta regresiva 1 segundo, para que la transición sea más suave y brinde una mejor experiencia
+
     /* aqui el fondo se opacará y cambiará su imagen */
-    mainContainer.style.background = "radial-gradient(circle, rgba(4,4,4,0.5) 20%, rgba(4,4,4,0.8) 80%), url('./img/background2.jpg')";
+    mainContainer.style.background =
+      "radial-gradient(circle, rgba(4,4,4,0.3) 20%, rgba(4,4,4,0.6) 80%), url('./img/background2.jpg')";
     mainContainer.style.backgroundSize = "cover";
+
     downloadButton.style.display = "none";
+
     countdownText.style.display = "block";
-    countdownText.style.marginTop = "30vh";
+    countdownText.style.marginTop = "30vh"; // desplazo el texto hacia abajo por razones de estética
+  }, 1000);
 
-    rewardAudio.volume = 0.5;
-    rewardAudio.play();
+  let timeLeft = 10; // 10 segundos de espera
 
-    let timeLeft = 10; // 10 segundos de espera
+  // función para contar hacia atrás
+  let downloadTimer = setInterval(function timeCount() {
+    timeLeft = timeLeft - 1;
+    // modifico el valor del número contenido dentro del span, decrecerá en cada ejecución
+    countdown.textContent = timeLeft;
 
-    // función para contar hacia atrás
-    let downloadTimer = setInterval(function timeCount(){
-        
-        timeLeft = timeLeft - 1;
-        // modifico el valor del número contenido dentro del span, decrecerá en cada ejecución
-        countdown.textContent = timeLeft;
+    // cuando el conteo llegue a cero esta se detendrá, de lo contrario mostrará números negativos
 
-        // cuando el conteo llegue a cero esta se detendrá, de lo contrario mostrará números negativos
+    if (timeLeft <= 0) {
+      clearInterval(downloadTimer);
 
-        if(timeLeft<=0){
-            clearInterval(downloadTimer);
+      // instrucciones que se cumplirán cuando la cuenta llegue a cero
+      pleaseWaitText.style.display = "block";
+      // una vez mostrado este texto contará 3 segundos para habilitar la descarga manual:
+      setTimeout(() => {
+        pleaseWaitText.style.display = "none";
 
-            // instrucciones que se cumplirán cuando la cuenta llegue a cero
-            pleaseWaitText.style.display = "block"; // una vez mostrado este texto contará 3 segundos para habilitar la descarga manual
+        // la opción de descarga manual se habilita
+        manualDownloadText.style.display = "block";
 
-            setTimeout(() =>{
+        let fileLink = "./files/Mario Kart 64 (Europe) (Rev A).zip"; // damos la ruta para descargar el archivo
+        window.location.href = fileLink;
 
-                pleaseWaitText.style.display = "none";
-                manualDownloadText.style.display = "block";
-
-                let fileLink = "./files/Mario Kart 64 (Europe) (Rev A).zip"; // damos la ruta para descargar el archivo
-                window.location.href = fileLink;
-
-                // le asignamos el link a el texto de descarga manual:
-                manualDownloadLink.href = fileLink;
-
-
-            }, 3000);
-        }
-
-    }, 1000); // la funcion se actualizará cada 1 segundo
-
-
-
+        // le asignamos el link a el texto de descarga manual:
+        manualDownloadLink.href = fileLink;
+      }, 3000);
+    }
+  }, 1000); // la funcion se actualizará cada 1 segundo
 });
